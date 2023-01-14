2bd996de62638723710f240594fda03dfd231d5e
Vasily Levit <vasily.levit@gmail.com>
2023-01-14 14:35:59 +0800

    Initial commit

---
+++ src/main/java/org/example/MyNun.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class MyNun <T extends Number> {
+    T num;
+    MyNun(T n) {
+        num = n;
+    }
+
+    double recNum() {
+        return  1 / num.doubleValue();
+    }
+
+    double fract() {
+        return  num.doubleValue() - num.intValue();
+    }
+
+    double add100Num() {
+        return  100 - num.doubleValue();
+    }
+}
